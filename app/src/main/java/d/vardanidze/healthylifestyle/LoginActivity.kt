f08d26481fa3272f6c3ba7269c37e3cec273e2e7
summ4 <mesumm4@gmail.com>
2021-06-20 12:26:16 +0400

    Done Auth services

---
+++ app/src/main/java/d/vardanidze/healthylifestyle/LoginActivity.kt
@@ -0,0 +1,90 @@
+package d.vardanidze.healthylifestyle
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var registerTextView: TextView
+    private lateinit var forgetPasswordTextView: TextView
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        if (mAuth.currentUser != null) {
+            goToMainActivity()
+        }
+
+        setContentView(R.layout.activity_login)
+
+        init()
+
+        registerListeners()
+
+    }
+
+    private fun init() {
+
+        emailEditText = findViewById(R.id.editTextTextEmailAddress)
+        passwordEditText = findViewById(R.id.editTextTextPassword)
+        loginButton = findViewById(R.id.loginbtn)
+        registerTextView = findViewById(R.id.textViewRegister)
+        forgetPasswordTextView = findViewById(R.id.textViewForgetPassword)
+
+    }
+
+    private fun registerListeners() {
+
+        loginButton.setOnClickListener {
+
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Empty!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        goToMainActivity()
+                    } else {
+                        Toast.makeText(this, "Authentication failed!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
+        }
+
+        registerTextView.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+
+        }
+
+
+        forgetPasswordTextView.setOnClickListener {
+            startActivity(Intent(this, PasswordResetActivity::class.java))
+        }
+
+    }
+
+    private fun goToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+}
